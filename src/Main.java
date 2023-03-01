b942ae7fc9582055c55b392b338348b05cf2596a
Keithcutt <mayerhofnew@gmail.com>
2023-03-01 22:30:02 +0600

    print array

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/* public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+} */
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[] {0, 1, 2, 3};
+        for (int j : arr) {
+            System.out.print(j + " ");
+        }
+    }
+}
+
